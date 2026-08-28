9121ce67c8348061ff285e5822d31e653f0b8ba7
Arina Zyranova <zyranovaarina02@gmail.com>
2026-08-28 16:32:41 +0400

    обновление  prisma schema для реализации поиска

---
+++ apps/chat-server/prisma/migrations/20260821114745_add_search_vector/migration.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "messages" ADD COLUMN "search_vector" tsvector GENERATED ALWAYS AS (to_tsvector('russian', coalesce("body_md", ''))) STORED;
+CREATE INDEX "messages_search_vector_idx" ON "messages" USING GIN ("search_vector");
