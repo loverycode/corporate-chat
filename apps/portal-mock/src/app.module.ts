d40c8653d9d80720d815b782050a3f417832ee70
Arina Zyranova <zyranovaarina02@gmail.com>
2026-08-28 16:43:54 +0400

    update portal mock

--- apps/portal-mock/src/app.module.ts
+++ apps/portal-mock/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ObjectsController } from './objects.controller';
 import { JwtModule } from '@nestjs/jwt';
+import {ServeStaticModule} from '@nestjs/serve-static';
+import {join} from 'path';
 
 @Module({
-    imports: [JwtModule.register({})],
+    imports: [JwtModule.register({}), ServeStaticModule.forRoot({rootPath: join(__dirname, '..', 'src', 'public')})],
     controllers: [AppController, ObjectsController],
 })
 export class AppModule {}
